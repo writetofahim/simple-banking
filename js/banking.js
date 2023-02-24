15b506c1834f82c6331827e74930cee20a2382b6
Fahim <writetofahimahammad@gmail.com>
2023-02-24 17:59:13 +0600

    complete banking

---
+++ js/banking.js
@@ -0,0 +1,35 @@
+function getInputValue(inputId) {
+  const inputField = document.getElementById(inputId);
+  const inputAmount = parseFloat(inputField.value);
+  inputField.value = "";
+  return inputAmount;
+}
+function UpdateTotal(totalId, inputAmount) {
+  const total = document.getElementById(totalId);
+  const totalAmount = parseFloat(total.innerText);
+  const newTotal = totalAmount + inputAmount;
+  total.innerText = newTotal;
+}
+function updateBalance(inputAmount, isAdd) {
+  const balanceField = document.getElementById("balance-total");
+  const balanceAmount = parseFloat(balanceField.innerText);
+  let newBalance;
+  if (isAdd == true) {
+    newBalance = balanceAmount + inputAmount;
+  } else {
+    newBalance = balanceAmount - inputAmount;
+  }
+  balanceField.innerText = newBalance;
+}
+//handle deposit event
+document.getElementById("deposit-btn").addEventListener("click", function () {
+  const inputAmount = getInputValue("deposit-input");
+  UpdateTotal("deposit-total", inputAmount);
+  updateBalance(inputAmount, true);
+});
+//handle withdraw event
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  const inputAmount = getInputValue("withdraw-input");
+  UpdateTotal("withdraw-total", inputAmount);
+  updateBalance(inputAmount, false);
+});
